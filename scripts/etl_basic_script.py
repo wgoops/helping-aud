e08f3a50db2d3a9ac93a4a3f15e5bf26b70fe459
wgoops <wgoops@gmail.com>
2023-10-22 21:19:07 -0400

    quick upload

---
+++ scripts/etl_basic_script.py
@@ -0,0 +1,28 @@
+#IMPORT STUFF
+
+
+####============ Requirements ============####
+
+#   What is the goal output
+#   What are some of the inputs that could help us get there
+#   What can be used as-is
+#   What requires a bit of additional elbow grease 
+
+####============ ETL STUFF ============####
+
+## Extract 
+
+#   0. Set up variables
+#   1. Bring in some files
+#   2. Get something from the files, and put it in a list/dict/whatever so that python can work with it
+
+## Transform
+
+#   3. 
+#   
+
+## Load
+
+#   
+
+# Get the thing
